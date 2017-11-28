67c40af89c1a48dadcc2de468bb1795edf84c04b
Don Yoo <dyoo@semigear.com>
2017-11-28 14:21:39 -0500

    server for android

---
+++ js/server/app.js
@@ -0,0 +1,36 @@
+require('dotenv').load();
+
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  passport = require('passport'),
+  flash = require('connect-flash'),
+  socketio = require('socket.io'),
+  morgan = require('morgan'),
+  cookieParser = require('cookie-parser'),
+  bodyParser = require('body-parser'),
+  session = require('express-session');
+
+mongoose.connect( process.env.MONGOLAB_URI, function(err) {
+    if (err) throw err;
+    else{
+      console.log('connect to mongodb');
+    }
+});
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({ secret: 'your secret key' }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+require('./config/passport')(passport);
+require('./routes')(app, passport);
+
+app.listen(port);
+console.log('Server running on port ' + port);
